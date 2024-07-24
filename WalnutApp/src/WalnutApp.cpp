c40d890bcc83f652da6c73b906e204ede99f2dc9
River-Salmon <40964273+River-Salmon@users.noreply.github.com>
2024-07-24 11:27:43 -0700

    Basic setup
    remove imgui demo window
    add stub menu items

--- WalnutApp/src/WalnutApp.cpp
+++ WalnutApp/src/WalnutApp.cpp
@@ -8,18 +8,15 @@ class ExampleLayer : public Walnut::Layer
 public:
 	virtual void OnUIRender() override
 	{
-		ImGui::Begin("Hello");
-		ImGui::Button("Button");
+		ImGui::Begin("main");
 		ImGui::End();
-
-		ImGui::ShowDemoWindow();
 	}
 };
 
 Walnut::Application* Walnut::CreateApplication(int argc, char** argv)
 {
 	Walnut::ApplicationSpecification spec;
-	spec.Name = "Walnut Example";
+	spec.Name = "HELL HATCH";
 
 	Walnut::Application* app = new Walnut::Application(spec);
 	app->PushLayer<ExampleLayer>();
@@ -28,6 +25,22 @@ Walnut::Application* Walnut::CreateApplication(int argc, char** argv)
 		if (ImGui::BeginMenu("File"))
 		{
 			if (ImGui::MenuItem("Exit"))
+			{
+				app->Close();
+			}
+			ImGui::EndMenu();
+		}
+		if (ImGui::BeginMenu("Mods"))
+		{
+			if (ImGui::MenuItem("Manage..."))
+			{
+				app->Close();
+			}
+			ImGui::EndMenu();
+		}
+		if (ImGui::BeginMenu("Multiplayer"))
+		{
+			if (ImGui::MenuItem("View Friends"))
 			{
 				app->Close();
 			}
