06e2398bc6c6e00ca4727bb38475240a3478e26c
River-Salmon <40964273+River-Salmon@users.noreply.github.com>
2024-07-24 17:16:53 -0700

    Hooking up game locator file dialog

--- WalnutApp/src/GameLocator/GameLocator.h
+++ WalnutApp/src/GameLocator/GameLocator.h
@@ -3,8 +3,12 @@
 #include <vector>
 #include <string>
 #include <filesystem>
+#include "tinyfiledialogs/tinyfiledialogs.h"
+#include "nano_signal_slot.hpp"
 
 #include "Walnut/Layer.h"
+
+Nano::Signal<void(std::filesystem::path)> GamePathProvidedSignal;
 
 class LocatorLayer : public Walnut::Layer
 {
@@ -12,10 +16,15 @@ public:
 	virtual void OnUIRender() override
 	{
 		ImGui::Begin("Locator");
-
+		ImGui::Text("Please provide the location of GZDoom.exe...");
 		if (ImGui::Button("Find...", ImVec2(100, 32)))
 		{
 			//TODO: open file dialog..
+			std::string pathstring = tinyfd_openFileDialog("Find Game", "", 0, NULL, NULL, 0);
+			std::filesystem::path path(pathstring);
+
+			//TODO: push a signal that indicates that gzdoom has been found at the provided path
+			GamePathProvidedSignal.fire(path);
 		}
 
 		ImGui::End();
@@ -32,14 +41,24 @@ class GameLocator
 {
 public:
 
-	GameLocator() {}
+	GameLocator() 
+	{
+		GamePathProvidedSignal.connect<&GameLocator::HandleGameFoundSignal>(this);
+	}
 	GameLocator(const GameLocator& Other) = delete;
 
 	void AutoDetect();
 	void ManuallyAddGame(ESupportedGames Game, const std::filesystem::path& GamePath);
 
 private:
-	std::vector<ESupportedGames> GamesToTryDetecting;
-	std::unordered_map<ESupportedGames, std::filesystem::path> DetectedGames;
+	//takes the path found by the file picker UI and applies it to the game settings
+	void HandleGameFoundSignal(std::filesystem::path NewGamePath)
+	{
+		GamePath = NewGamePath;
+		std::string WeGotHere;
+	}
+
+
+	std::filesystem::path GamePath;
 
 };
